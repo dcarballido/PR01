910461a2bf4b646c75c33a1b1b9312c5555b7e2d
diegocarballido <diego.carballido@icloud.com>
2019-11-05 18:12:20 +0100

    subida v2.1 --se suben ficheros de login php

--- php/login.php
+++ php/login.php
@@ -1,68 +1,47 @@
 <?php
-
-  // linkeamos db_connection.php para conexión a la base de datos
-  include "./db_connection.php";
-
+// linkeamos con db_connection para conectar a la base de datos
+include "./db_connection.php"; 
 ?>
 
 <!DOCTYPE html>
 <html>
 <head>
-  <link rel="stylesheet" type="text/css" href="estilos.css">
-  <script type="text/javascript" src="../../../front-end/js/login.js"></script>
-
+  <meta charset="utf-8">
   <title></title>
-
+  <script src="../js/login.js"></script>
 </head>
 <body>
-<section class="forms-section">
-  <h1 class="section-title">Animated Forms</h1>
-  <div class="forms">
-    <div class="form-wrapper is-active">
-      <button type="button" class="switcher switcher-login">
-        Login
-        <span class="underline"></span>
-      </button>
-      <form class="form form-login">
-        <fieldset>
-          <legend>Please, enter your email and password for login.</legend>
-          <div class="input-block">
-            <label for="login-email">E-mail</label>
-            <input id="login-email" type="email" required>
-          </div>
-          <div class="input-block">
-            <label for="login-password">Password</label>
-            <input id="login-password" type="password" required>
-          </div>
-        </fieldset>
-        <button type="submit" class="btn-login">Login</button>
-      </form>
-    </div>
-    <div class="form-wrapper">
-      <button type="button" class="switcher switcher-signup">
-        Sign Up
-        <span class="underline"></span>
-      </button>
-      <form class="form form-signup">
-        <fieldset>
-          <legend>Please, enter your email, password and password confirmation for sign up.</legend>
-          <div class="input-block">
-            <label for="signup-email">E-mail</label>
-            <input id="signup-email" type="email" required>
-          </div>
-          <div class="input-block">
-            <label for="signup-password">Password</label>
-            <input id="signup-password" type="password" required>
-          </div>
-          <div class="input-block">
-            <label for="signup-password-confirm">Confirm password</label>
-            <input id="signup-password-confirm" type="password" required>
-          </div>
-        </fieldset>
-        <button type="submit" class="btn-signup">Continue</button>
-      </form>
-    </div>
+  <div style="text-align: center; margin-top: 5%"><h1>Social Gallery</h1></div>
+  <div style="text-align: center; margin-top: 10%;">
+  <h1>Iniciar sesión</h1>
+  <form method="post" action="./19-10-21_projecte_1_social_gallery_m07_login.proc.php" onsubmit="first()">
+  <p>Recuerda, este formulario no acepta caracteres especiales!</p>
+    <?php
+    echo "<input pattern='[A-Za-z0-9_-]{1,15}' type='text' name='user' placeholder='Inserta el usuario' id='user' value='".$_REQUEST['loginuser']."'><br/>
+";
+    ?>
+    <input type="password" name="password" placeholder="Inserta el password" id="password" ><br/><br/>
+    <input type="submit" name="Enviar" value="Iniciar sesión" >
+  </form>
+
+  <?php
+    if ($_REQUEST['error'] == 1) {
+      echo "<p style = 'color: red;'>Usuario y/o contraseña incorrecto</p>";
+    }else{
+      echo "";
+    }
+  ?>
+
+  <h1>Registrarse</h1>
+  <form method="post" action="./19-10-21_projecte_1_social_gallery_m07_signin.proc.php" onsubmit="second()">
+  <p>Recuerda, este formulario no acepta caracteres especiales!</p>
+    <?php
+    echo "<input pattern='[A-Za-z0-9_-]{1,15}' type='text' name='user' placeholder='Inserta el usuario' id='user' value='".$_REQUEST['signinuser']."'><br/>
+";
+        ?>
+    <input pattern='[A-Za-z0-9_-]{1,15}' type="password" name="password" placeholder="Inserta el nuevo password" required><br/><br/>
+    <input pattern='[A-Za-z0-9_-]{1,15}' type="submit" name="Enviar" value="Registrarse" >
+  </form>
   </div>
-</section>
 </body>
 </html>
---
+++ php/login.proc.php
@@ -0,0 +1,31 @@
+<?php
+include "./db_connection.php";
+
+$user = $_REQUEST['user'];
+$pass = $_REQUEST['password'];
+$encript = md5($pass);
+
+//Entra si está configurada la variable del formulario del login
+if(isset($_REQUEST['user'])){
+
+	$query = "SELECT * FROM usuarios WHERE name_usuario='$user' AND password_usuario='$encript'";
+
+	$result = mysqli_query($conn,$query);
+	//La variable $result debería de tener como mínimo un registro coincidente
+	if(!empty($result) && mysqli_num_rows($result)==1){
+		$row = mysqli_fetch_array($result);
+		//Creo una nueva sesión y defino $_SESSION['nombre'] y $_SESSION['id_usuario']
+		session_start();
+		$_SESSION['nombre']=$user;
+		$_SESSION['id_usuario']=$row['id_usuario'];
+		//Voy a la intranet de la app
+		header("Location: ./intranet.php");
+	}else{
+		//Ha fallado la autenticación vuelvo a login.php
+		header("Location: ./login.php?error=1&loginuser=". $user);
+	}
+//Si no está configurada la variable del formulario del login vuelve al login.php
+}else{
+	header("Location: ./login.php");
+}	
+?>
