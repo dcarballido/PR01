1188cc963b2dde1c20a6804fcd72ea34aa544c75
Edgar <edgomoroter@hotmail.com>
2019-11-07 15:40:25 +0100

    Subida v2.7 --Subida de intranet.php completa + filtrar.php

--- php/filtrar.php
+++ php/filtrar.php
@@ -19,47 +19,43 @@
 <?php  
 include 'db_connection.php';
 
-$query_s="SELECT * FROM tbl_salas ORDER BY nom_sala DESC";
+if (!isset($_GET['salas']) && !isset($_GET['recursos']) ) {
+	$sala=$_REQUEST['salas'];
+$query_fs1="SELECT * FROM tbl_salas WHERE nom_sala='$sala'";
 
-$result_s=mysqli_query($conn,$query_s);
+$result_fs1=mysqli_query($conn,$query_fs1);
 
-while ($row = mysqli_fetch_array($result_s)) {
+while ($row1 = mysqli_fetch_array($result_fs1)) {
 		echo "<tr>
-			<td>".$row['nom_sala']."</td><td>".$row['tlf_sala']."</td><td>".$row['edificio_sala']."</td>
+			<td>".$row1['nom_sala']."</td><td>".$row1['tlf_sala']."</td><td>".$row1['edificio_sala']."</td>
 		</tr>";
 		
 }
-		
-?>
-		</table>
-<br><br>
-		<table>
+echo "<table>
 			<thead>
 				<tr>
 					<th>ID</th><th>Nombre</th><th>Tipo</th>
 				</tr>
-			</thead>
-
-			<?php  
-
-$query_r="SELECT * FROM tbl_recursos ORDER BY id_recurs ASC";
-
-$result_r=mysqli_query($conn,$query_r);
-
-while ($row = mysqli_fetch_array($result_r)) {
+			</thead>";
+$recurs=$_REQUEST['recursos'];
+$query_fr1="SELECT * FROM tbl_recursos WHERE nom_recurs='$recurs'";
+$result_fr1=mysqli_query($conn,$query_fr1);
+while ($row2 = mysqli_fetch_array($result_fr1)) {
 		echo "<tr>
-			<td>".$row['id_recurs']."</td><td>".$row['nom_recurs']."</td><td>".$row['tipo_recurs']."</td>
+			<td>".$row2['id_recurs']."</td><td>".$row2['nom_recurs']."</td><td>".$row2['tipo_recurs']."</td>
 		</tr>";
 		
 }
-		
+
+		}
 ?>
 		</table>
+<br><br>
 		
 </div>		
 <div id="cajafiltro">
-<div id="filtro">	
-			<h2 style="background-color: #ED820A; color: white; width: 833%; margin-bottom: 10px;">Filtros</h2>
+<div id="filter">	
+			<h2 style="background-color: #ED820A; color: white; width: 833%; margin-top: -700%;">Filtros</h2>
 			
 			<form action="filtrar.php" method="post">
 				<a><select name="salas">
@@ -84,7 +80,7 @@ while ($row = mysqli_fetch_array($result_r)) {
 				<input style="margin-left: 8px; width: 740%;" type="reset" value="Borrar"/> 
 			</form>
 		<h2 style="background-color: #ED820A; color: white; width: 833%; margin-bottom: 10px;">Reservas</h2>
-		<a href="mis_reservas.php" style="margin-left: 40px;"><button>MIS RESERVAS</button></a><br><br>
+		<a href="mis_reservas.php" style="margin-left: 32px;"><button>MIS RESERVAS</button></a><br><br>
 </div>	
 </div>			
 </body>
